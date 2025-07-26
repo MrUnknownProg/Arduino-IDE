f2e5ad6ff2a0b176caceb2eaaf80af8248f15e4d
Rutuj Rotkar <147727459+MrUnknownProg@users.noreply.github.com>
2025-07-26 18:55:43 +0530

    Add files via upload

---
+++ Get_IP_Address/GetIP.ino
@@ -0,0 +1,74 @@
+/*  Connects to the home WiFi network
+ *  Asks some network parameters
+ *  Starts WiFi server with fix IP and listens
+ *  Receives and sends messages to the client
+ *  Communicates: wifi_client_01.ino
+ */
+#include <SPI.h>
+#include <ESP8266WiFi.h>
+
+byte ledPin = 14;
+char ssid[] = "";               // SSID of your home WiFi
+char pass[] = "";               // password of your home WiFi
+WiFiServer server(80);                    
+
+IPAddress ip(192,168,172,11);            // IP address of the server
+IPAddress gateway(192,168,172,12);           // gateway of your network
+IPAddress subnet(255,255,255,0);          // subnet mask of your network
+IPAddress allowedClientIP1(192,168,172,230);
+IPAddress allowedClientIP2(192,168,172,89);
+void setup() {
+  Serial.begin(115200);                   // only for debug
+  WiFi.config(ip, gateway, subnet);       // forces to use the fix IP
+  WiFi.begin(ssid, pass);                 // connects to the WiFi router
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+  server.begin();                         // starts the server
+ Serial.println("Connected to wifi");
+  Serial.print("Status: "); Serial.println(WiFi.status());  // some parameters from the network
+  Serial.print("IP: ");     Serial.println(WiFi.localIP());
+  Serial.print("Subnet: "); Serial.println(WiFi.subnetMask());
+  Serial.print("Gateway: "); Serial.println(WiFi.gatewayIP());
+  Serial.print("SSID: "); Serial.println(WiFi.SSID());
+  Serial.print("Signal: "); Serial.println(WiFi.RSSI());
+  Serial.print("Networks: "); Serial.println(WiFi.scanNetworks());
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop () {
+  WiFiClient client = server.available();
+  
+  if (client) {
+    IPAddress clientIP = client.remoteIP();
+    if (clientIP == allowedClientIP1) {
+      if (client.connected()) {
+      digitalWrite(ledPin, LOW);  // to show the communication only (inverted logic)
+      String request = client.readStringUntil('\r');    // receives the message from the client
+      Serial.print("From client: "); Serial.println(request);
+      client.flush();
+      client.println("Got it Node A.\r"); // sends the answer to the client
+      digitalWrite(ledPin, HIGH);
+    }
+    }
+    else if (clientIP == allowedClientIP2) {
+      if (client.connected()) {
+      digitalWrite(ledPin, LOW);  // to show the communication only (inverted logic)
+      Serial.println(".");
+      String request = client.readStringUntil('\r');    // receives the message from the client
+      Serial.print("From client: "); Serial.println(request);
+      client.flush();
+      client.println("Got it Node B.\r"); // sends the answer to the client
+      digitalWrite(ledPin, HIGH);
+    }
+    }
+     else{
+      Serial.println("NOT");
+      
+      }
+    
+        
+    client.stop();                // tarminates the connection with the client
+  }
+}
